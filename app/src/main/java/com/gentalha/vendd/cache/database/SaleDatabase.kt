3da727a93b333aaa5f6ff1c6c2caf79eff6135b7
thgMatajs <tprmatajs@gmail.com>
2024-03-20 18:14:40 -0300

    adjust type converter

--- app/src/main/java/com/gentalha/vendd/cache/database/SaleDatabase.kt
+++ app/src/main/java/com/gentalha/vendd/cache/database/SaleDatabase.kt
@@ -2,10 +2,13 @@ package com.gentalha.vendd.cache.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.gentalha.vendd.cache.converter.ProductConverter
 import com.gentalha.vendd.cache.dao.SaleDao
 import com.gentalha.vendd.cache.entity.SaleEntity
 
 @Database(entities = [SaleEntity::class], version = 1)
+@TypeConverters(ProductConverter::class)
 abstract class SaleDatabase : RoomDatabase() {
 
     abstract fun saleDao(): SaleDao
--- app/src/main/java/com/gentalha/vendd/cache/entity/SaleEntity.kt
+++ app/src/main/java/com/gentalha/vendd/cache/entity/SaleEntity.kt
@@ -2,12 +2,11 @@ package com.gentalha.vendd.cache.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.gentalha.vendd.model.Product
 import com.gentalha.vendd.model.Sale
 
 @Entity(tableName = "sales")
 data class SaleEntity(
     @PrimaryKey(autoGenerate = true) override val id: Long,
     override val clientName: String,
-    override val products: List<Product>,
+    override val products: List<ProductEntity>,
 ) : Sale
